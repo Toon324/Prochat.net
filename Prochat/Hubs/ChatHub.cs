f20ecaf575e30d00bd0507b4ba60425246b57edf
Cody Swendrowski <cody@swendrowski.us>
2014-12-07 20:41:02 -0600

    Adds a way to showcase all current features.

--- Prochat/Hubs/ChatHub.cs
+++ Prochat/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
@@ -28,8 +29,25 @@ namespace Prochat.Hubs
 
         private string ParseMessage(string message)
         {
-            if (message.Contains("youtu.be") || message.Contains("youtube.com/watch?"))
+            if (message.Equals("Prochat, show me your features"))
+            {
+                Send("Prochat", "Hello, and welcome to Prochat! Here are the current awesome features you can enjoy:");
+                
+                Send("Prochat", "You can also paste most Youtube video links and have them automagically embed. If a video isn't working, try using the link found in the \"Share\" tab on the video page.");
+                Send("Prochat", "http://youtu.be/SQoA_wjmE9w");
+
+                Send("Prochat", "Images such as png, jpg, and gif are all embedded as well. All embeded objects can be hidden and shown using the link found in the message.");
+                Send("Prochat", "http://i.imgur.com/p49J3rc.gif");
+
+                Send("Prochat", "Other links will simply be linked in the message");
+                Send("Prochat", "http://www.google.com");
+               
+                
+            }
+            else if (message.Contains("youtu.be") || message.Contains("youtube.com/watch?"))
                 message = HandleYoutube(message);
+            else if (message.Contains(".png") || message.Contains(".jpg") || message.Contains(".gif"))
+                message = HandleImage(message);
             else if (message.Contains("http"))
                 message = HandleLink(message);
             
@@ -38,7 +56,7 @@ namespace Prochat.Hubs
 
         private string HandleLink(string message)
         {
-            var url = message.Substring(message.IndexOf("http"), message.IndexOf(".com")+4);
+            var url = message.Substring(message.IndexOf("http:"), message.IndexOf(".com")+4);
             var wrapped = "<a target=\"_blank\" href=\"" + url + "\" >" + url + "</a>";
 
             message = message.Replace(url, wrapped);
@@ -56,10 +74,19 @@ namespace Prochat.Hubs
             if (message.Contains("youtu.be"))
                 data = message.Substring(message.IndexOf("youtu.be") + 9);
             else
-                data = message.Substring(message.IndexOf("?v=") + 3);
+                data = message.Substring(message.IndexOf("v=") + 2);
 
             message = Embed("Youtube Video ", "<embed width=\"420\" height=\"315\" src=\"http://www.youtube.com/v/" + data + "\">");
    
+            return message;
+        }
+
+        private string HandleImage(string message)
+        {
+            var url = Regex.Match(message, @"http://.+\..../.+\....").ToString();
+            var embedded = "<img src=\"" + url + "\" style=\"width: 400px; height:300px\">";
+            message = message.Replace(url, Embed("Image ", embedded));
+
             return message;
         }
 
