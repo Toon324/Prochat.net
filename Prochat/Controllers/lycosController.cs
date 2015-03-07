fabb0e1523c264602ed17dd61517bd8d3cd6121a
Cody Swendrowski <cody@swendrowski.us>
2015-03-07 16:03:23 -0600

    Lycos easter egg!
    ADD: "Let me LYCOS that for you" easter egg

---
+++ Prochat/Controllers/lycosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Prochat.Models;
+
+namespace Prochat.Controllers
+{
+    public class lycosController : Controller
+    {
+        // GET: lycos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search(string id)
+        {
+            var model = new LycosModel {SearchTerm = id};
+            return View(model);
+        }
+    }
+}
